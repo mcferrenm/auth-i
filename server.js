474d1c59077867248dbc28e08c442ce6acca7b83
mcferrenm <mcferrenmax@gmail.com>
2019-02-25 15:55:36 -0500

    Add register Users route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
+const bcrypt = require("bcryptjs");
 
 const Users = require("./users/UsersModel");
 
@@ -16,6 +17,23 @@ server.get("/api/users", async (req, res) => {
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ error: "Error retrieving users" });
+  }
+});
+
+server.post("/api/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    if (!username || !password) {
+      res.status(400).json({ message: "Must provide username and password" });
+    } else {
+      const hash = bcrypt.hashSync(password, 12);
+      req.body.password = hash;
+
+      const [id] = await Users.add(req.body);
+      res.status(201).json({ id });
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Error registering user" });
   }
 });
 
--- users/UsersModel.js
+++ users/UsersModel.js
@@ -3,5 +3,8 @@ const db = require('../data/knexConfig');
 module.exports = {
   find: function() {
     return db('users')
+  },
+  add: function(credentials) {
+    return db('users').insert(credentials)
   }
 };
