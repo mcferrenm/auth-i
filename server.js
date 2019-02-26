0d0cfc07e69f059f0708d76688cf16e651b85639
mcferrenm <mcferrenmax@gmail.com>
2019-02-26 15:30:31 -0500

    Add sessions to login

--- server.js
+++ server.js
@@ -2,36 +2,44 @@ const express = require("express");
 const helmet = require("helmet");
 const bcrypt = require("bcryptjs");
 const cors = require("cors");
+const session = require("express-session");
 
 const Users = require("./users/UsersModel");
 
 const server = express();
 
+const sessionConfig = {
+  name: "monkey",
+  secret: "my secret monkey secret",
+  cookie: {
+    maxAge: 1000 * 60 * 15, // 15 min
+    secure: false // https only!
+  },
+  httpOnly: true, // no js access through document.cookie
+  resave: false, // don't resave unmodified
+  saveUninitialized: false // GDPR laws against setting cookies automatically w/o consent
+};
+
 // Global Middleware
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
+server.use(session(sessionConfig));
 
 // Local Middleware
 
 async function restricted(req, res, next) {
-  const { username, password } = req.headers;
-  try {
-    const user = await Users.findBy({ username });
-    if (user && bcrypt.compareSync(password, user.password)) {
-      next();
-    } else {
-      res.status(401).json({ message: "Invalid Credentials" });
-    }
-  } catch (error) {
-    res.status(500).json({ error: "Error logging in user" });
+  if (req.session && req.session.userId) {
+    next();
+  } else {
+    res.status(401).json({ message: "Not authorized" });
   }
 }
 
 server.use("/api/restricted", restricted);
 
 // Routes
-server.get("/api/restricted/users", async (req, res) => {
+server.get("/api/restricted/users", restricted, async (req, res) => {
   try {
     const users = await Users.find();
     res.status(200).json(users);
@@ -40,14 +48,14 @@ server.get("/api/restricted/users", async (req, res) => {
   }
 });
 
-server.get("/api/users", async (req, res) => {
-  try {
-    const users = await Users.find();
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Error retrieving users" });
-  }
-});
+// server.get("/api/users", async (req, res) => {
+//   try {
+//     const users = await Users.find();
+//     res.status(200).json(users);
+//   } catch (error) {
+//     res.status(500).json({ error: "Error retrieving users" });
+//   }
+// });
 
 server.post("/api/register", async (req, res) => {
   const { username, password } = req.body;
@@ -74,12 +82,10 @@ server.post("/api/login", async (req, res) => {
     } else {
       const user = await Users.findBy({ username });
       if (user && bcrypt.compareSync(password, user.password)) {
-        res
-          .status(200)
-          .json({
-            message: `Welcome ${user.username}`,
-            jwt: "987asdhjk12389dddd"
-          });
+        req.session.userId = user.id;
+        res.status(200).json({
+          message: `Welcome ${user.username}`
+        });
       } else {
         res.status(401).json({ message: "Invalid Credentials" });
       }
